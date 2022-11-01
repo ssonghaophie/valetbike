2620528f056a9871e3c6afe90fffb486b73c9e75
Sophie Song <ssong25@smith.edu>
2022-11-01 13:18:59 -0400

    trip db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_023914) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_01_032214) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -22,6 +22,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_023914) do
     t.integer "identifier"
     t.string "name"
     t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "docked_bike_count"
+  end
+
+  create_table "trips", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "trip_id"
+    t.string "user_id"
+    t.integer "current_station_id"
+    t.integer "startTime"
+    t.integer "endTime"
+    t.string "review"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
