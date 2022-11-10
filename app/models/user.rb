406c1cfc15d15b548cf0c4e0614a87a06cbe88ad
violetbi <violetbi@github.com>
2022-11-09 23:22:45 -0500

    add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,18 @@ class User < ApplicationRecord
     has_secure_password
     # validates email
     validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
- end
+    
+    PASSWORD_FORMAT = /\A
+        (?=.{8,})          # Must contain 8 or more characters
+        (?=.*\d)           # Must contain a digit
+        (?=.*[a-z])        # Must contain a lower case character
+        (?=.*[A-Z])        # Must contain an upper case character
+        (?=.*[[:^alnum:]]) # Must contain a symbol
+    /x
+    validates :password, 
+        presence: true, length: { in: 8..20 }, 
+        format: { with: PASSWORD_FORMAT }, 
+        confirmation: true, 
+        on: :create 
+
+end
