de2ef5be6afb6161746a19ccb751813465dc6de2
Evelyn Gao <ygao@smith.edu>
2022-11-10 22:01:08 -0500

    signin/up style

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] =  user.id
   end
-    # 返回当前登录的用户（如果有的话）
+ 
   def current_user
       @current_user ||= User.find_by(id: session[:user_id])
   end
